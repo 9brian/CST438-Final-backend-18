1f95956ae2ae43735fcd97c3fc646287e3889e40
jessica <jbejarano@csumb.edu>
2023-11-15 15:02:41 -0800

    create branch dev_j

--- src/main/java/com/cst438/Domain/FlightDTO.java
+++ src/main/java/com/cst438/Domain/FlightDTO.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public record FlightDTO(int flight_id, String carrier_name, LocalDateTime departure_time,
                         LocalDateTime arrival_time, String location, String destination,
-                        double price) {
+                        double price )  {
 
 }
 //    CREATE TABLE flights_table (
