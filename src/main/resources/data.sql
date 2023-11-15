7baf06ce65cc2dd21273be967b15d3e12e13accb
Jbaires831 <jbaires616@gmail.com>
2023-11-15 14:28:54 -0800

    database setup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+-- Inserting data into the 'user' table
+INSERT INTO user_table (id, email, full_name, password) VALUES
+                                                            (1, 'john.doe@example.com', 'John Doe', 'password_1'),
+                                                            (2, 'jane.smith@example.com', 'Jane Smith', 'password_2');
+
+-- Inserting data into the 'flights' table -WILL BE USING API-
+INSERT INTO flights_table (flight_id, carrier_name, departure_time, arrival_time, location, destination, price) VALUES
+                                                                                                                    (1000, 'Delta', '2023-11-20 12:00:00', '2023-11-20 15:00:00', 'Los Angeles', 'Atlanta', 250.00),
+                                                                                                                    (2000, 'AeroMexico', '2023-11-21 10:30:00', '2023-11-21 13:30:00', 'Nicks House', 'Mexico City', 300.00);
+
+-- Inserting data into the 'scheduled_flights' table
+INSERT INTO scheduled_flights_table (booking_number, email, flight_no, carrier_name) VALUES
+                                                                                         (1,'john.doe@example.com', 1000, 'Delta'),
+                                                                                         (2,'jane.smith@example.com', 2000, 'AeroMexico');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE user_table (
+                            id            INT AUTO_INCREMENT,
+                            email         VARCHAR(45) UNIQUE,
+                            full_name     VARCHAR(25) NOT NULL,
+                            password      VARCHAR(100) NOT NULL,
+                            PRIMARY KEY (id)
+);
+
+CREATE TABLE flights_table (
+                               flight_id       INT,
+                               carrier_name    VARCHAR(100),
+                               departure_time  DATETIME NOT NULL,
+                               arrival_time    DATETIME NOT NULL,
+                               location        VARCHAR(100) NOT NULL,
+                               destination     VARCHAR(100) NOT NULL,
+                               price           DECIMAL(10,2) NOT NULL,
+                               PRIMARY KEY (flight_id, carrier_name)
+);
+
+CREATE TABLE scheduled_flights_table (
+                                         booking_number  INT AUTO_INCREMENT,
+                                         email           VARCHAR(45),
+                                         flight_no       INT,
+                                         carrier_name    VARCHAR(100),
+                                         PRIMARY KEY (booking_number),
+                                         FOREIGN KEY (email) REFERENCES user_table(email) ON DELETE CASCADE,
+                                         FOREIGN KEY (flight_no, carrier_name) REFERENCES flights_table(flight_id, carrier_name) ON DELETE CASCADE
+);
